89f46989736fa581d364eb6b5f1a73aeb84363fb
Vanya's Win_10 <1vasilisk12@gmail.com>
2021-12-04 19:09:20 +0300

    quadratic equation (simple)

---
+++ qu_eq.py
@@ -0,0 +1,20 @@
+import math
+ 
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+ 
+discr = b ** 2 - 4 * a * c
+print(f"Дискриминант D = {discr}")
+ 
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print(f"x1 = {x1} \nx2 = {x2}")
+elif discr == 0:
+    x = -b / (2 * a)
+    print(f"x = {x}")
+else:
+    print("Корней нет")
